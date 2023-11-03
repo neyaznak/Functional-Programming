3b28351fb91c300c7169e1f4a26c7d321d061670
arala <arala@Nawaz-ROG>
2023-11-03 18:17:31 +0530

    First Commit

---
+++ src/programming/FP01Functional.java
@@ -0,0 +1,55 @@
+package programming;
+
+import java.util.List;
+
+public class FP01Functional {
+
+	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        // printAllNumbersInListFunctional(numbers);
+		// printEvenNumbersInListFunctional(numbers);
+		// printSquaresOfEvenNumbersInListFunctional(numbers);
+		printCubesOfOddNumbersInListFunctional(numbers);
+
+	}
+
+	private static void printCubesOfOddNumbersInListFunctional(List<Integer> numbers) {
+
+		numbers.stream().filter(number -> number % 2 != 0).map(number -> number * number * number)
+				.forEach(System.out::println);
+
+	}
+
+//	private static void print(int number) {
+//		System.out.println(number);
+//	}
+
+//	private static boolean isEven(int number) {
+//		return number % 2 == 0;
+//	}
+
+	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+		// What to do?
+
+		numbers.stream().forEach(System.out::println);// Method Reference
+
+	}
+
+	// number -> number%2 == 0
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+		// What to do?
+		numbers.stream().filter(number -> number % 2 == 0)// Lambda Expression
+				.forEach(System.out::println);// Method Reference
+		// .filter(FP01Functional::isEven)// Filter - Only Allow Even Numbers
+	}
+
+	private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+		// What to do?
+		numbers.stream().filter(number -> number % 2 == 0)// Lambda Expression
+				// mapping - x -> x * x
+				.map(number -> number * number).forEach(System.out::println);// Method Reference
+		// .filter(FP01Functional::isEven)// Filter - Only Allow Even Numbers
+	}
+
+}
